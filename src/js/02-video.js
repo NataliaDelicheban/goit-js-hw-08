b057126d94baf7f18e94bbcc4240aee7d215ce93
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-06-06 12:25:32 +0200

    change task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,17 @@
-
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector("iframe");
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
-const onPlay = function (data) {
-    const value = data.seconds;
-    const pauseTime = JSON.stringify(value);
-    localStorage.setItem("videoplayer-current-time", pauseTime);
-};
+const key = 'videoplayer-current-time';
+const savedTime = localStorage.getItem(key) || 0;
+//console.log(savedTime);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
+function onPlay(data) {
+  const value = data.seconds;
+  localStorage.setItem(key, value);
+}
 
+player.setCurrentTime(savedTime);
