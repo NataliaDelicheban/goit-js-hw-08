d3c302e992973db304fd6342790f24255006e7bc
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-06-07 21:21:40 +0200

    fix the bug task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,14 +5,27 @@ const email = document.querySelector('input');
 const message = document.querySelector('textarea');
 const formKey = 'feedback-form-state';
 
-let formData = {};
-
+let formData = rebootForm() ?? {};
+rebootForm();
 form.addEventListener('submit', cleanForm);
 form.addEventListener('input', throttle(saveValue, 500));
 
 function saveValue(e) {
   formData[e.target.name] = e.target.value;
   localStorage.setItem(formKey, JSON.stringify(formData));
+}
+
+function rebootForm() {
+  const formTextContent = localStorage.getItem(formKey) || '';
+  if (formTextContent) {
+    const parsedTextContent = JSON.parse(formTextContent);
+    email.value = parsedTextContent.email;
+    message.value = parsedTextContent.message;
+  }
+  return {
+    email: email.value,
+    message: message.value,
+  };
 }
 
 function cleanForm(e) {
@@ -22,16 +35,7 @@ function cleanForm(e) {
   if (email.value === '' || message.value === '') {
     return alert('Please fill out the form!');
   }
-
   console.log(formData);
   localStorage.removeItem(formKey);
   form.reset();
-}
-
-function rebootForm() {
-  const formTextContent = localStorage.getItem(formKey) || '';
-  if (formTextContent) {
-    message.value = formTextContent.message;
-    email.value = formTextContent.email;
-  }
 }
